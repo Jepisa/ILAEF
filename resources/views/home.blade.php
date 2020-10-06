3953a13f06fc2ffe065f947ed3b99dc796f7915f
francogiorgio <6104610franco@gmail.com>
2020-10-06 12:30:34 -0300

    Responsive navbar 1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,17 +11,52 @@
 @section('content')
 
 <div id="div-fondo-superior">
-    <nav id="navbar">
-        <div>
-            <img id="logo-ILAEF" scr="{{ asset('img/logo-ILAEF.jpg') }}" >
-        </div>
-        <div id="menu">
-            <span>MENÜ</span>
-        </div>
-    </nav>
+
+{{-- agrego aca lo del navbar que estaba en la pagina de prueba - Franco --}}
+<nav class="nav-index">
+        <div class="nav-background">  
+                
+            {{-- header mobile --}}
+            <div class="header-desktop">
+                <div class="div-logo">
+                    <img class="logo-ILAEF" src="{{ asset('img/Logos-ILAEF.jpg') }}" alt="ILAEF">
+                </div>
+                <div class="div-enlaces">
+                    <a class="a-home" href=""><img src="https://www.flaticon.es/premium-icon/icons/svg/2168/2168260.svg" alt="home" style="width: 30px !important; height: 24px; padding: 0 2px;"></a>
+                    <a href="">Quiénes Somos</a>
+                    <a href="">Nuestra misión</a>
+                    <a href="">Agenda</a>
+                    <a href="">Eventos</a>
+                    <a href="">Biblioteca</a>
+                </div> 
+            </div>
+            {{-- header mobile --}}
+            <div class="header-mobile">
+                <div class="div-logo-mobile">
+                    <img class="logo-ILAEF-mobile" src="{{ asset('img/Logos-ILAEF.jpg') }}" alt="ILAEF">
+                </div>
+                <div class="div-enlaces-mobile">
+                    <ul class="menu_items">
+                        <li><a href="">Quiénes Somos</a></li>
+                        <li><a href="">Nuestra misión</a></li>
+                        <li><a href="">Agenda</a></li>
+                        <li><a href="">Eventos</a></li>
+                        <li><a href="">Biblioteca</a></li>
+                    </ul>    
+                    
+                </div> 
+                <span class="menu_mobile">
+                   <i><img src="{{ asset('img/menu_mobile.png') }}" alt="" class="menu_mobile_logo"></i>
+                </span>
+                
+                    
+                
+            </div>
+        </nav>
 </div>
 
 {{-- Carousel --}}
+<div class="carousel">
 <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active">
@@ -47,7 +82,7 @@
       </div>
     </div>
 </div>
-
+</div>
 {{-- Paises en los globos (Parte Media) --}}
 <div id="div-fondo-medio">
     <img id="img-logo-medio" src="{{ asset('img/circulos 2.jpg') }}" alt="logo-ILAEF">
@@ -123,6 +158,7 @@
 
 @section('script')
     <!-- JS, Popper.js, and jQuery -->
+    <script src="../js/app.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
