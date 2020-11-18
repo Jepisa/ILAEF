028d667dcdc4df4e827e4b33839ef291ff11e4c9
Jepisa <javierjeanpieres@gmail.com>
2020-11-18 11:17:02 -0300

    add opciones en para el usuario en el navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -138,10 +138,113 @@
             margin: 5px;
         }
     }
-</style>
 
+    /* -------------------------- */
+    .text-sm {
+    font-size: 0.875rem;
+}
+
+.border-2 {
+    border-width: 2px;
+}
+.border-transparent {
+    border-color: transparent;
+}
+.rounded-full {
+    border-radius: 9999px;
+}
+.focus\:outline-none:focus {
+    outline: 2px solid transparent;
+    outline-offset: 2px;
+}
+.focus\:border-gray-300:focus {
+    --border-opacity: 1;
+    border-color: #d2d6dc;
+    border-color: rgba(210, 214, 220, var(--border-opacity));
+}
+
+.transition {
+    transition-property: background-color, border-color, color, fill, stroke, opacity, box-shadow, transform;
+}
+
+.duration-150 {
+    transition-duration: 150ms;
+}
+.ease-in-out {
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+}
+
+
+.h-8 {
+    height: 2rem;
+}
+.object-cover {
+    object-fit: cover;
+}
+
+.miami div .shadow-lg{
+    position: absolute;
+    z-index: 1;
+    margin-left: -153px;
+}
+
+</style>
+<div class="miami d-flex justify-content-end">
+    @auth
+        <x-jet-dropdown align="right" width="48">
+            <x-slot name="trigger">
+                @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                    <button class="d-flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
+                        <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                    </button>
+                @else
+                    <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                        <div>{{ Auth::user()->name }}</div>
+
+                        <div class="ml-1">
+                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                    </button>
+                @endif
+            </x-slot>
+
+            <x-slot name="content">
+                <!-- Account Management -->
+                <div class="block px-4 py-2 text-xs text-gray-400">
+                    {{ __('Manage Account') }}
+                </div>
+
+                <x-jet-dropdown-link href="{{ route('profile.show') }}">
+                    {{ __('Profile') }}
+                </x-jet-dropdown-link>
+
+                @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
+                    <x-jet-dropdown-link href="{{ route('api-tokens.index') }}">
+                        {{ __('API Tokens') }}
+                    </x-jet-dropdown-link>
+                @endif
+
+                <div class="border-t border-gray-100"></div>
+
+                <!-- Authentication -->
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+
+                    <x-jet-dropdown-link href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                        {{ __('Logout') }}
+                    </x-jet-dropdown-link>
+                </form>
+            </x-slot>
+        </x-jet-dropdown>
+    @endauth
+</div>
 <div class="divNav">
     <div class="navbar" id="navbar">
+
         <div class="logos">
             <div class="logoNavBar">
                 <a href="{{ route('home') }}">
@@ -171,9 +274,11 @@
                     <a href="/admin">Admin</a>
                 </li>
                 @endauth
+                
             </ul>
         </div>
     </div>
+    
 </div>
 
 <script>
