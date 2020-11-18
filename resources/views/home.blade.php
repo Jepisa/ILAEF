2f3cbd1dcfa20e137c575868ede0fd6f60f9636c
Jepisa <javierjeanpieres@gmail.com>
2020-11-18 08:57:44 -0300

    add la funcion que solo nuestre cards si hay news

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,39 +3,42 @@
   <canvas class="conection"></canvas>
   <x-carrousel />
   <x-globos />
-   <div class="divCards container">
-    <div class="row">
-      @foreach($news as $new)
-       <!-- CARD -->
-       <div class="col-12 col-md-6 col-lg-4" data-aos="fade-in" data-aos-duration="1000" data-aos-delay="50">
-        <a href="{{$new->url}}" id="url">
-          <div class="cardNew">
-            <div class="imgNew">
-              <div class="tagPais"></div>
-              <img src="{{$new->image}}" alt="">
+  @isset($news)
+    <div class="divCards container">
+      <div class="row">
+        @foreach($news as $new)
+        <!-- CARD -->
+        <div class="col-12 col-md-6 col-lg-4" data-aos="fade-in" data-aos-duration="1000" data-aos-delay="50">
+          <a href="{{$new->url}}" id="url">
+            <div class="cardNew">
+              <div class="imgNew">
+                <div class="tagPais"></div>
+                <img src="{{$new->image}}" alt="">
+              </div>
+              <div class="body">
+                <div class="title">
+                  <p>
+                    {{$new->title}}
+                  </p>
+                </div>
+                <div class="description">
+                  <p>
+                    {{$new->description}}
+                  </p>
+                </div>
+                <div class="reloj">
+                    <img src='/icons/reloj.png' alt="" srcset="">
+                    <p>{{$new->created_at->format('d/m/Y')}}</p>
+                </div>
+              </div>
             </div>
-            <div class="body">
-              <div class="title">
-                <p>
-                  {{$new->title}}
-                </p>
-              </div>
-              <div class="description">
-                <p>
-                  {{$new->description}}
-                </p>
-              </div>
-              <div class="reloj">
-                  <img src='/icons/reloj.png' alt="" srcset="">
-                  <p>{{$new->created_at->format('d/m/Y')}}</p>
-              </div>
-            </div>
-          </div>
-        </a>
+          </a>
+        </div>
+        @endforeach
       </div>
-      @endforeach
+  
     </div>
-  </div>
+  @endisset
 </x-app-layout>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/particlesjs/2.2.3/particles.min.js"></script>
 <script src=" https://unpkg.com/aos@next/dist/aos.js"></script>
