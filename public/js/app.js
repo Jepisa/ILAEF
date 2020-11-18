f00c84c52167cc6f590f487806132d4d54432829
Jepisa <javierjeanpieres@gmail.com>
2020-11-18 08:49:40 -0300

    pequeños cambios auto-generados

--- public/js/app.js
+++ public/js/app.js
@@ -37265,6 +37265,17 @@ module.exports = function(module) {
 
 /***/ }),
 
+/***/ "./resources/css/app.css":
+/*!*******************************!*\
+  !*** ./resources/css/app.css ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ "./resources/js/app.js":
 /*!*****************************!*\
   !*** ./resources/js/app.js ***!
@@ -37333,14 +37344,15 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /***/ }),
 
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!*************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/css/app.css ***!
+  \*************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Leonel\Desktop\ILAEF\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Leonel\Desktop\ILAEF\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\javie_000\Desktop\FACULTAD 2do CUATRI 2020\Proyecto Voluntariado\ILAEF\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! C:\Users\javie_000\Desktop\FACULTAD 2do CUATRI 2020\Proyecto Voluntariado\ILAEF\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! C:\Users\javie_000\Desktop\FACULTAD 2do CUATRI 2020\Proyecto Voluntariado\ILAEF\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
